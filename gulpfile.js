17a770403a6022a7d402de938b469f675cf43c29
Jeremiah Long <jml@jmldev.com>
2019-01-11 14:52:58 -0800

    Finished setup for handlebars static compile

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var connect = require('gulp-connect');
+const hb = require("gulp-hb");
 
 sass.compiler = require('node-sass');
 
 gulp.task('sass', function () {
-	return gulp.src('./sass/**/*.scss')
+	return gulp.src('./src/sass/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('./css'));
+		.pipe(gulp.dest('./dist/css'));
 });
 
 gulp.task('connect', function (cb) {
@@ -26,8 +27,20 @@ gulp.task('html', function (cb) {
 	cb();
 });
 
+gulp.task('handlebars', function() {
+	return gulp
+		.src('./src/*.html')
+		.pipe(hb()
+			.partials('./src/partials/**/*.hbs')
+			.helpers('./src/helpers/*.js')
+			.data('./src/data/**/*.{js,json}')
+		)
+		.pipe(gulp.dest('./dist'))
+});
+
 gulp.task('watch', function (cb) {
+	gulp.watch('./src/**/*.hbs', gulp.series('handlebars'));
 	gulp.watch('./dist/*.html', gulp.series('html'));
-	gulp.watch('./sass/*.scss', gulp.series('sass'));
+	gulp.watch('./src/sass/*.scss', gulp.series('sass'));
 	cb();
 });

f37056653f152429ade8f797a11008a5527c27ef
Jeremiah Long <jeremlong@expediagroup.com>
2019-06-03 11:48:17 -0700

    added browser prefixing and concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,34 @@
 'use strict';
 
+/* global require */
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var connect = require('gulp-connect');
-const hb = require("gulp-hb");
-const minify = require("gulp-minify");
+var hb = require("gulp-hb");
+var minify = require("gulp-minify");
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
 sass.compiler = require('node-sass');
 
 gulp.task('sass', function () {
 	return gulp.src('./src/sass/**/*.scss')
+		.pipe(sourcemaps.init())
 		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('./dist/css'))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('./src/sass/css'))
 		.pipe(connect.reload());
+});
+
+gulp.task('prefix', function() {
+	return gulp.src('./src/sass/css/**/*.css')
+	.pipe(sourcemaps.init())
+	.pipe(autoprefixer({
+		browsers: ['last 2 versions']
+	}))
+	.pipe(concat('all.css'))
+	.pipe(sourcemaps.write('.'))
+	.pipe(gulp.dest('./dist/css/'));
 });
 
 gulp.task('connect', function (cb) {
@@ -36,13 +53,13 @@ gulp.task('handlebars', function () {
 			.helpers('./src/helpers/*.js')
 			.data('./src/data/**/*.{js,json}')
 		)
-		.pipe(gulp.dest('./dist'))
+		.pipe(gulp.dest('./dist'));
 });
 
 gulp.task('js', function (cb) {
 	gulp.src('src/js/*.js')
 		.pipe(minify())
-		.pipe(gulp.dest('dist/js/'))
+		.pipe(gulp.dest('dist/js/'));
 	cb();
 });
 
@@ -54,14 +71,17 @@ gulp.task('move', function (cb) {
 
 gulp.task('build', function (cb) {
 	gulp.series('sass');
+	gulp.series('prefix');
 	gulp.series('handlebars');
 	gulp.series('move');
+	gulp.series('js');
 	cb();
 });
 
 gulp.task('watch', function (cb) {
 	gulp.watch(['./src/**/*.hbs', './src/*.html'], gulp.series('handlebars'));
 	gulp.watch('./src/sass/*.scss', gulp.series('sass'));
+	gulp.watch('./src/sass/css/*.css', gulp.series('prefix'));
 	gulp.watch('./src/img/**/*.*', gulp.series('move'));
 	gulp.watch('./src/js/*.js', gulp.series('js'));
 	gulp.watch("./dist/**/*.*", gulp.series("reload"));

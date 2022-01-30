4b2a76ece846cc59e8d5fbbbf5cf165f850637b1
Jeremiah Long <jml@jmldev.com>
2022-01-30 14:12:26 -0800

    cleaned up some config

--- gulpfile.js
+++ gulpfile.js
@@ -2,33 +2,25 @@
 
 /* global require */
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('node-sass'));
 var connect = require('gulp-connect');
 var hb = require("gulp-hb");
 var minify = require("gulp-minify");
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
-var concat = require('gulp-concat');
-sass.compiler = require('node-sass');
+var concat = require('gulp-concat');``
 
 gulp.task('sass', function () {
 	return gulp.src('./src/sass/**/*.scss')
 		.pipe(sourcemaps.init())
 		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['>2%', 'last 2 versions']
+		}))
+		.pipe(minify())
 		.pipe(sourcemaps.write())
-		.pipe(gulp.dest('./src/sass/css'))
+		.pipe(gulp.dest('./dist/css'))
 		.pipe(connect.reload());
-});
-
-gulp.task('prefix', function() {
-	return gulp.src('./src/sass/css/**/*.css')
-	.pipe(sourcemaps.init())
-	.pipe(autoprefixer({
-		browsers: ['last 2 versions']
-	}))
-	.pipe(concat('all.css'))
-	.pipe(sourcemaps.write('.'))
-	.pipe(gulp.dest('./dist/css/'));
 });
 
 gulp.task('connect', function (cb) {
@@ -70,18 +62,13 @@ gulp.task('move', function (cb) {
 });
 
 gulp.task('build', function (cb) {
-	gulp.series('sass');
-	gulp.series('prefix');
-	gulp.series('handlebars');
-	gulp.series('move');
-	gulp.series('js');
+	gulp.series(['sass', 'handlebars', 'move', 'js']);
 	cb();
 });
 
 gulp.task('watch', function (cb) {
 	gulp.watch(['./src/**/*.hbs', './src/*.html'], gulp.series('handlebars'));
 	gulp.watch('./src/sass/*.scss', gulp.series('sass'));
-	gulp.watch('./src/sass/css/*.css', gulp.series('prefix'));
 	gulp.watch('./src/img/**/*.*', gulp.series('move'));
 	gulp.watch('./src/js/*.js', gulp.series('js'));
 	gulp.watch("./dist/**/*.*", gulp.series("reload"));
